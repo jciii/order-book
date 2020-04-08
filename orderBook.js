6e4d3711c356fd43c90f616c8aadf862ff6ba272
James Cardinale <jms.card9.jc@gmail.com>
2020-04-08 17:03:49 -0400

    answer-with-hardcode

--- orderBook.js
+++ orderBook.js
@@ -8,50 +8,59 @@ function reconcileOrder(existingBook, incomingOrder) {
 
     return updatedBook = existingBook.concat(incomingOrder)
   }
-  if (sameType[0] === incomingOrder.type) {
+  else if (sameType[0] === incomingOrder.type) {
 
     return updatedBook = existingBook.concat(incomingOrder)
   }
-  if (sameType != incomingOrder.type && existingBook.length < 2) {
+  else if (sameType != incomingOrder.type && existingBook.length < 2) {
 
     return updatedBook = existingBook.concat(incomingOrder)
   }
-  if (samePrice === incomingOrder.price && sameType[0] != incomingOrder.type) {
+  else if (samePrice === incomingOrder.price && sameType[0] != incomingOrder.type) {
 
     return updatedBook = existingBook.concat(incomingOrder)
   }
-  if (sameQuantity[0] === incomingOrder.quantity && samePrice[0] === incomingOrder.price) {
+  else if (sameQuantity[0] === incomingOrder.quantity && samePrice[0] === incomingOrder.price) {
 
     return updatedBook = existingBook.slice(1)
   }
-  if (sameQuantity[0] > incomingOrder.quantity && sameType[0] != incomingOrder.type) {
+  else if (sameQuantity[0] > incomingOrder.quantity && sameType[0] != incomingOrder.type) {
     existingBook[0].quantity = sameQuantity[0] - incomingOrder.quantity
     updatedBook = existingBook.reverse()
 
     return updatedBook
   }
-  if (sameQuantity[0] + sameQuantity[1] >= incomingOrder.quantity && existingBook.length <= 2) {
+  else if (sameQuantity[0] < incomingOrder.quantity && existingBook.length <= 2) {
     incomingOrder.quantity = incomingOrder.quantity - sameQuantity[0]
     updatedBook = updatedBook.concat(incomingOrder, existingBook[1])
 
     return updatedBook.reverse()
   }
-  if (incomingOrder.quantity === sameQuantity[0] + sameQuantity[1]) {
+  else if (incomingOrder.quantity === sameQuantity[0] + sameQuantity[1]) {
 
     return updatedBook = existingBook.slice(2)
   }
-  if (sameQuantity[0] + sameQuantity[1] >= incomingOrder.quantity && samePrice[0, 1] === incomingOrder.price) {
+  else if (sameType[0] === sameType[1] && sameQuantity[0] + sameQuantity[1] >= incomingOrder.quantity && samePrice[0, 1] === incomingOrder.price) {
     existingBook[0].quantity = ((sameQuantity[0] + sameQuantity[1]) - incomingOrder.quantity)
     existingBook.concat(existingBook[0].quantity)
     updatedBook = existingBook.filter((items => items.quantity != sameQuantity[0])).reverse()
 
     return updatedBook
   }
-  if (sameQuantity[0] + sameQuantity[1] <= incomingOrder.quantity && samePrice[0, 1] === incomingOrder.price) {
+  else if (sameQuantity[0] + sameQuantity[1] <= incomingOrder.quantity && samePrice[0, 1] === incomingOrder.price) {
     incomingOrder.quantity = incomingOrder.quantity - (sameQuantity[0] + sameQuantity[1])
     updatedBook = existingBook.concat(incomingOrder)
     updatedBook = updatedBook.slice(2)
 
+    return updatedBook
+  }
+  else if (sameQuantity[0] === incomingOrder.quantity && sameType[0] != incomingOrder.type && samePrice[0] > incomingOrder.price) {
+    updatedBook = existingBook.slice(1)
+    return updatedBook
+  }
+  else if (sameQuantity[0] === incomingOrder.quantity && sameType[0] != incomingOrder.type && samePrice[0] < incomingOrder.price) {
+    updatedBook = existingBook
+    updatedBook.push(incomingOrder)
     return updatedBook
   }
 }
